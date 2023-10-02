80f9bd649705b9c737d805660c3fac684300810f
14089 <baoxiong814@gmail.com>
2023-10-01 20:12:40 -0500

    added more questions and connected countdown  with gameEnd

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ backButton.addEventListener("click", function () {
     document.location.reload();
 });
 
-
+//when start button is clicked, questions pop up
 startButton.addEventListener("click", startGame);
 
 function startGame() {
@@ -25,23 +25,49 @@ function startGame() {
     intro.setAttribute("class", "is-inactive");
 }
 
-var firstQuestion = document.getElementById("first-question"); 
-var secondQuestion = document.getElementById("second-question"); 
-var thirdQuestion = document.getElementById("third-question"); 
-var fourthQuestion = document.getElementById("fourth-question"); 
-var fifthQuestion = document.getElementById("fifth-question"); 
-
-function Question1 () {
-    firstQuestion.setAttribute ("class", "is-active");
-    secondQuestion.setAttribute ("class", "is-inactive");
-    thirdQuestion.setAttribute ("class", "is-inactive");
-    fourthQuestion.setAttribute ("class", "is-inactive");
-    fifthQuestion.setAttribute ("class", "is-inactive");
-}
-
 //function to tell if chosen answer is correct/wrong AND
 //also moves on to the next question in 1 second ON
 //CLICK of an answer
+
+// var answerButton = document.getElementsByClassName("answer-button");
+// answerButton.addEventListener("click", ;
+
+
+var currentQuestion = 0;
+
+var loadQuestions = [
+    {
+        question: "Where do we link JavaScript in the index.html?",
+        answer: [
+            {choice: "In the head.", answer: false}, 
+            {choice: "In the beginning of the body.", answer: false},
+            {choice: "At the end of the body.", answer: true},
+            {choice: "In the footer.", answer: false}
+            
+        ]
+    }, 
+    {
+        question: "What does location.reload(); do?",
+        answer: [
+            {choice: "It takes you back to the homepage.", answer: false},
+            {choice: "It forces the page to reload.", answer: true},
+            {choice: "It stops the page from reloading.", answer: false},
+            {choice: "It forces a page to reload when you reach a specific page.", answer: false}
+        ]
+    },
+    {
+        question: "Which of the following stores data even after a browser has closed and/or computer has restarted?",
+        answer: [
+            {choice: "Cookies", answer: false},
+            {choice: "sessionStorage", answer: false},
+            {choice: "Cache Storage", answer: false},
+            {choice: "localStorage", answer: true}
+        ]
+    },
+];
+
+//push questions and answers to choices html
+
 
 function endGame() {
     game.setAttribute("class", "is-inactive");
@@ -55,17 +81,25 @@ function viewHiScore() {
     hiScore.setAttribute("class", "is-active");
 }
 
-var timeLeft = 60;
-var elem = document.getElementById('Timer');
+//when timer ends, message pops up to end game and show user
+//their high score and show the option to go back to intro page
+var timeLeft = 5;
+var time = document.getElementById('Timer');
 
-var timerId = setInterval(countdown, 1000);
+
+game.addEventListener("click", function() {
+    var timer = setInterval(countdown, 1000);
+});
 
 function countdown() {
   if (timeLeft == 0) {
-    clearTimeout(timerId);
-    doSomething();
+    gameEnd.setAttribute("class", "is-active");
+    hiScore.setAttribute("class", "is-active");
+    preGame.setAttribute("class", "is-inactive");
+    startButton.setAttribute("class", "is-inactive");
+    intro.setAttribute("class", "is-inactive");
   } else {
-    elem.innerHTML = timeLeft + ' seconds remaining';
+    time.innerHTML = timeLeft + ' seconds remaining';
     timeLeft--;
   }
 }
